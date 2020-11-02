49be51b849a9b6227c93c6c0600764631476635f
Jeffrey Perkel <j_perkel@yahoo.com>
2020-11-02 07:40:16 -0700

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 #[macro_use]
 extern crate gb_io;
 
-use std::fs::File;
 use std::collections::HashMap;
+use std::env;
+use std::fs::File;
 use std::io;
 use std::io::Write;
 use std::process;
-use std::env;
 
 use gb_io::reader::SeqReader;
 
-// Translation table 11 from https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi. 
-// Indexing into this array is as follows: 
+// Translation table 11 from https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi.
+// Indexing into this array is as follows:
 // T = 0, C = 1, A = 2, G = 3
-// index = (16 * first_base) + (4 * second_base) + third_base 
+// index = (16 * first_base) + (4 * second_base) + third_base
 // So... TTT = 0, TTC = 1, TTA = 2, ... , GGC = 61, GGA = 62, GGG = 63
 const GENETIC_CODE: &str = "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG";
 
-// ERR_BAD_NT is an error value for an invalid nucleotide 
-const ERR_BAD_NT: usize = 99; 
+// ERR_BAD_NT is an error value for an invalid nucleotide
+const ERR_BAD_NT: usize = 99;
 
 // enumeration of genetic sequence types
 enum SeqType {
@@ -47,38 +47,41 @@ fn lookup(x: char) -> usize {
 // translate a codon into its corresponding amino acid
 fn translate(triplet: &str, t: Translation) -> String {
     let three_letter_code: HashMap<char, &str> = [
-    ('A', "Ala"), 
-    ('B', "???"), 
-    ('C', "Cys"), 
-    ('D', "Asp"), 
-    ('E', "Glu"), 
-    ('F', "Phe"), 
-    ('G', "Gly"), 
-    ('H', "His"), 
-    ('I', "Ile"), 
-    ('J', "???"), 
-    ('K', "Lys"), 
-    ('L', "Leu"), 
-    ('M', "Met"), 
-    ('N', "Asn"), 
-    ('O', "Pyr"), 
-    ('P', "Pro"), 
-    ('Q', "Gln"), 
-    ('R', "Arg"), 
-    ('S', "Ser"), 
-    ('T', "Thr"), 
-    ('U', "Sel"), 
-    ('V', "Val"), 
-    ('W', "Trp"), 
-    ('X', "???"), 
-    ('Y', "Tyr"), 
-    ('Z', "???"), 
-    ('*', "***"), 
-    ].iter().copied().collect();
+        ('A', "Ala"),
+        ('B', "???"),
+        ('C', "Cys"),
+        ('D', "Asp"),
+        ('E', "Glu"),
+        ('F', "Phe"),
+        ('G', "Gly"),
+        ('H', "His"),
+        ('I', "Ile"),
+        ('J', "???"),
+        ('K', "Lys"),
+        ('L', "Leu"),
+        ('M', "Met"),
+        ('N', "Asn"),
+        ('O', "Pyr"),
+        ('P', "Pro"),
+        ('Q', "Gln"),
+        ('R', "Arg"),
+        ('S', "Ser"),
+        ('T', "Thr"),
+        ('U', "Sel"),
+        ('V', "Val"),
+        ('W', "Trp"),
+        ('X', "???"),
+        ('Y', "Tyr"),
+        ('Z', "???"),
+        ('*', "***"),
+    ]
+    .iter()
+    .copied()
+    .collect();
 
     let mut codon = vec![ERR_BAD_NT; 3];
 
-    for (i,base) in triplet.chars().enumerate() {
+    for (i, base) in triplet.chars().enumerate() {
         codon[i] = lookup(base);
     }
 
@@ -104,14 +107,14 @@ fn print_seq(s: &str, t: SeqType) {
     let linelen = 72;
 
     let divisor = match t {
-        // if we're printing a Protein3, count amino acids, not bases, 
+        // if we're printing a Protein3, count amino acids, not bases,
         // so divide by 3
-        SeqType::DNA | SeqType::Protein1 =>  1,
+        SeqType::DNA | SeqType::Protein1 => 1,
         SeqType::Protein3 => 3,
     };
 
     // how many lines to print
-    let mut nlines = s.len()/linelen;
+    let mut nlines = s.len() / linelen;
     // if there's a remainder, add one line.
     if s.len() % linelen != 0 {
         nlines += 1;
@@ -119,26 +122,31 @@ fn print_seq(s: &str, t: SeqType) {
 
     // print the lines
     for i in 0..nlines {
-        let start = i*linelen;
-        let mut end = (i*linelen) + linelen;
+        let start = i * linelen;
+        let mut end = (i * linelen) + linelen;
         if end > s.len() {
             end = s.len();
         }
         let myline = &s[start..end];
-        println!("{number:>0width$} {}", myline, number=((start/divisor))+1, width=count_digits(s.len() as u16));
+        println!(
+            "{number:>0width$} {}",
+            myline,
+            number = (start / divisor) + 1,
+            width = count_digits(s.len() as u16)
+        );
     }
 }
 
-// NOTE: n is type u16, so allowable input is 0..65535. 
+// NOTE: n is type u16, so allowable input is 0..65535.
 fn count_digits(mut n: u16) -> usize {
     let mut digits: usize = 1;
 
     while n > 0 {
-        n = n/10;
+        n = n / 10;
         if n >= 1 {
             digits += 1;
         }
-    };
+    }
 
     return digits;
 }
@@ -174,38 +182,45 @@ fn main() {
         for f in &seq.features {
             feat_count += 1;
             // collect protein_id, product, and location data for each "CDS", ie gene
-            if f.kind==feature_kind!("CDS") {
-                let gene = f.qualifier_values(qualifier_key!("protein_id"))
+            if f.kind == feature_kind!("CDS") {
+                let gene = f
+                    .qualifier_values(qualifier_key!("protein_id"))
                     .next()
                     .unwrap()
                     .to_string()
-                    .replace('\n',"");
+                    .replace('\n', "");
                 genes.push(gene);
 
-                let desc = f.qualifier_values(qualifier_key!("product"))
+                let desc = f
+                    .qualifier_values(qualifier_key!("product"))
                     .next()
                     .unwrap()
                     .to_string()
-                    .replace('\n',"");
+                    .replace('\n', "");
                 descs.push(desc);
 
                 locs.push(f.location.clone());
                 gene_count += 1;
             }
         }
-        println!("\nFound {} features, including {} genes.", feat_count, gene_count);
+        println!(
+            "\nFound {} features, including {} genes.",
+            feat_count, gene_count
+        );
         for i in 0..gene_count {
-            println!("{}) {}: {}", i, genes[i],descs[i]);
+            println!("{}) {}: {}", i, genes[i], descs[i]);
         }
 
         if gene_count > 0 {
-            print!("\nWhich would you like to view [0-{}]: ", gene_count-1);
+            print!("\nWhich would you like to view [0-{}]: ", gene_count - 1);
             // flush buffer...
             io::stdout().flush().unwrap();
 
             // get input...
             let mut retval = String::new();
-            io::stdin().read_line(&mut retval).expect("Failed to read from stdin");
+            io::stdin()
+                .read_line(&mut retval)
+                .expect("Failed to read from stdin");
 
             // use trim() to delete the trailing newline ('\n') char
             retval = retval.trim().to_string();
@@ -215,10 +230,10 @@ fn main() {
                 Err(_) => {
                     println!("Invalid input: '{}'", retval);
                     process::exit(1);
-                },
+                }
             };
 
-            if selection > gene_count-1 {
+            if selection > gene_count - 1 {
                 println!("Invalid input: '{}'", selection);
                 process::exit(1);
             }
@@ -226,23 +241,30 @@ fn main() {
             println!("You selected: {}", selection);
 
             for f in &seq.features {
-                if f.kind==feature_kind!("CDS") {
-                    if f.qualifier_values(qualifier_key!("protein_id")).next().unwrap() == genes[selection] {
-                        let s = String::from_utf8(seq.extract_location(&locs[selection]).unwrap().to_vec())
-                            .unwrap()
-                            .to_ascii_uppercase();
+                if f.kind == feature_kind!("CDS") {
+                    if f.qualifier_values(qualifier_key!("protein_id"))
+                        .next()
+                        .unwrap()
+                        == genes[selection]
+                    {
+                        let s = String::from_utf8(
+                            seq.extract_location(&locs[selection]).unwrap().to_vec(),
+                        )
+                        .unwrap()
+                        .to_ascii_uppercase();
 
                         println!("\nDNA sequence:");
                         print_seq(&s, SeqType::DNA);
                         println!("Length: {}\n", s.len());
-        
-                        let mut peptide1 = String::new();    
-                        let mut peptide3 = String::new();    
-                        let n_codons = s.len()/3;
+
+                        let mut peptide1 = String::new();
+                        let mut peptide3 = String::new();
+                        let n_codons = s.len() / 3;
                         for i in 0..n_codons {
-                            let codon = &s[i*3..(i*3)+3]; // take a 3-base slice of the sequence
-                            peptide1.push_str(&translate(&codon,    Translation::OneLetter)); // translate and add to the string
-                            peptide3.push_str(&translate(&codon, Translation::ThreeLetter)); // translate and add to the string
+                            let codon = &s[i * 3..(i * 3) + 3]; // take a 3-base slice of the sequence
+                            peptide1.push_str(&translate(&codon, Translation::OneLetter)); // translate and add to the string
+                            peptide3.push_str(&translate(&codon, Translation::ThreeLetter));
+                            // translate and add to the string
                         }
                         println!("One-letter code:");
                         print_seq(&peptide1, SeqType::Protein1);
@@ -255,7 +277,6 @@ fn main() {
         }
     }
 }
-
 
 // tests
 #[cfg(test)]
@@ -287,4 +308,3 @@ mod tests {
         assert_eq!(count_digits(2500), 4);
     }
 }
-
